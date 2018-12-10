84cc66b7b75833db6efcd8fc42890360f32d513e
punisher077 <arthur.mts@gmail.com>
2018-12-10 13:48:14 -0300

    .

--- Projeto BD e POO;/src/Identifiable.java
+++ Projeto BD e POO;/src/Identifiable.java
@@ -1,28 +1,29 @@
 import javax.persistence.Id;
 
-public class Identifiable {
- @Id
- private Long id;
- private String nome;
- private String hospede;
-public Long getId() {
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public String getNome() {
-	return nome;
-}
-public void setNome(String nome) {
-	this.nome = nome;
-}
-public String getHospede() {
-	return hospede;
-}
-public void setHospede(String hospede) {
-	this.hospede = hospede;
-}
+		public class Identifiable {
+		@Id
+ 		private Long id;
+ 		private String nome;
+ 		private String hospede;
+ 
+ 		public Long getId() {
+ 			return id;
+ 		}
+ 		public void setId(Long id) {
+ 			this.id = id;
+ 		}
+ 		public String getNome() {
+ 			return nome;
+ 		}
+ 		public void setNome(String nome) {
+ 			this.nome = nome;
+ 		}
+ 		public String getHospede() {
+ 			return hospede;
+ 		}
+ 		public void setHospede(String hospede) {
+ 			this.hospede = hospede;
+ 		}
  
 }
 
